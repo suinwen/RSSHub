d63ec6a422d48129c51d759f8dbeb325ca230232
suinwen <suinwen@gmail.com>
2026-06-25 11:49:16 +0800

    Update news.ts

--- lib/routes/cbp/news.ts
+++ lib/routes/cbp/news.ts
@@ -1,8 +1,28 @@
-const html = await ofetch(url, {
-    headers: {
-        'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-    },
-});
+import { load } from 'cheerio';
 
-throw new Error(html.substring(0, 1000));
+import type { Route } from '@/types';
+import { ViewType } from '@/types';
+import ofetch from '@/utils/ofetch';
+
+export const route: Route = {
+    path: '/news',
+    name: 'CBP',
+    maintainers: ['suinwen'],
+    example: '/cbp/news',
+    categories: ['government'],
+    view: ViewType.Articles,
+
+    handler: async () => {
+        const url = 'https://www.cbp.gov/newsroom/media-releases/all';
+
+        const html = await ofetch(url);
+
+        const $ = load(html);
+
+        return {
+            title: 'CBP',
+            link: url,
+            item: [],
+        };
+    },
+};
