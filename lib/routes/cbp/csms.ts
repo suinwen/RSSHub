ba47812fbc3d41d995c6a1a2ee88a044609a74e1
suinwen <suinwen@gmail.com>
2026-07-28 14:44:29 +0800

    Update csms.ts

--- lib/routes/cbp/csms.ts
+++ lib/routes/cbp/csms.ts
@@ -1,61 +1,66 @@
-import { Route } from '@/types';
+import { load } from 'cheerio';
 
+import type { Route } from '@/types';
+import { ViewType } from '@/types';
 import ofetch from '@/utils/ofetch';
 import { parseDate } from '@/utils/parse-date';
-import * as cheerio from 'cheerio';
 
 export const route: Route = {
-    path: '/cbp/csms',
-    categories: ['government'],
-    example: '/cbp/csms',
-    features: {
-        requireConfig: false,
-        requirePuppeteer: false,
-        antiCrawler: false,
-        supportBT: false,
-        supportPodcast: false,
-        supportScihub: false,
-    },
-    radar: [
-        {
-            source: ['www.cbp.gov/trade/automated/cargo-systems-messaging-service'],
-        },
-    ],
-    name: 'CBP Cargo Systems Messaging Service',
+    path: '/csms',
+    name: 'Cargo Systems Messaging Service',
     maintainers: ['suinwen'],
-    handler,
+    example: '/cbp/csms',
+    categories: ['government'],
+    view: ViewType.Articles,
+
+    handler: async () => {
+        const rootUrl = 'https://www.cbp.gov/trade/automated/cargo-systems-messaging-service';
+        const apiUrl = 'https://content.govdelivery.com/accounts/USDHSCBP/widgets/USDHSCBP_WIDGET_2.json';
+
+        const items = await ofetch(apiUrl);
+
+        const limit = 30;
+        const detailItems = (
+            await Promise.all(
+                items.slice(0, limit).map(async (item: any) => {
+                    try {
+                        const html = await ofetch(item.href, {
+                            timeout: 10000,
+                            headers: {
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+                            },
+                        });
+
+                        const $ = load(html);
+                        const bodyEl = $('#bulletin_body');
+                        bodyEl.find('style').remove();
+
+                        return {
+                            title: item.subject,
+                            description: bodyEl.html() ?? item.subject,
+                            link: item.href,
+                            pubDate: parseDate(item.pub_date),
+                            guid: item.href,
+                        };
+                    } catch (error) {
+                        console.log(`Skip: ${item.href}`);
+
+                        return {
+                            title: item.subject,
+                            link: item.href,
+                            pubDate: parseDate(item.pub_date),
+                            guid: item.href,
+                        };
+                    }
+                })
+            )
+        ).filter(Boolean);
+
+        return {
+            title: 'CBP Cargo Systems Messaging Service (CSMS)',
+            link: rootUrl,
+            description: 'U.S. Customs and Border Protection - Cargo Systems Messaging Service',
+            item: detailItems,
+        };
+    },
 };
-
-async function handler() {
-    const rootUrl = 'https://www.cbp.gov/trade/automated/cargo-systems-messaging-service';
-    const apiUrl = 'https://content.govdelivery.com/accounts/USDHSCBP/widgets/USDHSCBP_WIDGET_2.json';
-
-    const items: { subject: string; pub_date: string; href: string }[] = await ofetch(apiUrl);
-
-    const limit = 30;
-    const detailItems = await Promise.all(
-        items.slice(0, limit).map(async (item) => {
-            const response = await ofetch(item.href);
-            const $ = cheerio.load(response);
-            const bodyEl = $('#bulletin_body');
-            bodyEl.find('style').remove();
-            const description = bodyEl.html() || item.subject;
-
-            return {
-                title: item.subject,
-                description,
-                link: item.href,
-                pubDate: parseDate(item.pub_date),
-                guid: item.href,
-            };
-        })
-    );
-
-    return {
-        title: 'CBP Cargo Systems Messaging Service (CSMS)',
-        link: rootUrl,
-        description: 'U.S. Customs and Border Protection - Cargo Systems Messaging Service',
-        language: 'en-us',
-        item: detailItems,
-    };
-}
